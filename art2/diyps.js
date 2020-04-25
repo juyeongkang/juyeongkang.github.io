eeebfc92774a0ae58974007189e6cd781b0b7a34
juyeongkang <61995839+juyeongkang@users.noreply.github.com>
2020-04-25 14:39:47 -0700

    add codeart2

--- art2/diyps.js
+++ art2/diyps.js
@@ -1,126 +1,127 @@
 var img;
-var initials ='JK'; // your initials
+var initials = 'JK'; // your initials
 var choice = '1'; // starting choice, so it is not empty
 var screenbg = 250; // off white background
-var lastscreenshot=61; // last screenshot never taken
+var lastscreenshot = 61; // last screenshot never taken
 
 function preload() {
-// preload() runs once, it may make you wait
-//  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js file
-// you can link to an image on your github account
-  img = loadImage('https://dma-git.github.io/images/74.png');
-  img2 = loadImage('https://juyeongkang.github.io/house4.jpg');
-  img3 = loadImage('https://juyeongkang.github.io/tower1.jpg');
+    // preload() runs once, it may make you wait
+    //  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js file
+    // you can link to an image on your github account
+    img = loadImage('https://dma-git.github.io/images/74.png');
+    img2 = loadImage('https://juyeongkang.github.io/house4.jpg');
+    img3 = loadImage('https://juyeongkang.github.io/tower1.jpg');
 }
 
 function setup() {
-createCanvas(600, 600);  // canvas size
-background(255);   // use our background screen color
+    createCanvas(600, 600); // canvas size
+    background(255); // use our background screen color
 
 }
 
 function draw() {
-  if (keyIsPressed) {
-    choice = key; // set choice to the key that was pressed
-    clear_print(); // check to see if it is clear screen or save image
-  }
-  if (mouseIsPressed){
-    newkeyChoice(choice);  // if the mouse is pressed call newkeyChoice
-  }
+    if (keyIsPressed) {
+        choice = key; // set choice to the key that was pressed
+        clear_print(); // check to see if it is clear screen or save image
+    }
+    if (mouseIsPressed) {
+        newkeyChoice(choice); // if the mouse is pressed call newkeyChoice
+    }
 }
 
 function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
-  // the key mapping if statements that you can change to do anything you want.
-  // just make sure each key option has the a stroke or fill and then what type of 
-  // graphic function
+    // the key mapping if statements that you can change to do anything you want.
+    // just make sure each key option has the a stroke or fill and then what type of 
+    // graphic function
 
- if (toolChoice == '1' ) {  // first tool
-   
-    background(0);
-    
-  } else if (toolChoice == '2') { // second tool
+    if (toolChoice == '1') { // first tool
 
-    stroke(255, 204, 0);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (toolChoice == '3') { // third tool
+        background(0);
 
-    ellipseMode(RADIUS); 
-    fill(255); //
-    ellipse(mouseX, mouseY, 40, 40, 30, 30); 
+    } else if (toolChoice == '2') { // second tool
 
-    ellipseMode(CENTER); 
-    noStroke(20)
-    fill(255, 204, 0);
-    ellipse(mouseX, mouseY, 30, 30, 40, 40);
-  } else if (toolChoice == '4') {
+        stroke(255, 204, 0);
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    } else if (toolChoice == '3') { // third tool
 
-    stroke(0, 0, 255);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (key == '5') { // this tool calls a function
-    stroke(135,206,235);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    
-    
-    
-    
-    // make testbox do something!
- //   line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (toolChoice == '6') {
+        ellipseMode(RADIUS);
+        fill(255); //
+        ellipse(mouseX, mouseY, 40, 40, 30, 30);
 
-    ellipseMode(RADIUS); 
-    fill(255, 190, 0); //
-    ellipse(mouseX, mouseY, 35, 34, 26, 24); 
+        ellipseMode(CENTER);
+        noStroke(20)
+        fill(255, 204, 0);
+        ellipse(mouseX, mouseY, 30, 30, 40, 40);
+    } else if (toolChoice == '4') {
 
-    ellipseMode(CENTER); 
-    noStroke(20)
-    fill(255, 204, 0);
-    ellipse(mouseX, mouseY, 30, 30, 40, 40);
-  } else if (toolChoice == '7') {
+        stroke(0, 0, 255);
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    } else if (key == '5') { // this tool calls a function
+        stroke(135, 206, 235);
+        line(mouseX, mouseY, pmouseX, pmouseY);
 
-    image(img2, mouseX, mouseY);
-  } else if (toolChoice == '8') {
-    image(img3, mouseX, mouseY);
 
-  } else if (toolChoice == '9') {
-    noStroke(20)
-    ellipse(mouseX, mouseY, 53, 46, 55, 55)
-    
-  } else if (toolChoice == '0') {
-    stroke(0, 0);
-    fill(random(255), random(255), random(255), random(255));
-    rect(mouseX, mouseY, 70, 130);
-  } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
-    image(img, mouseX, mouseY);
-    
-  }
- }
- 
+
+
+        // make testbox do something!
+        //   line(mouseX, mouseY, pmouseX, pmouseY);
+    } else if (toolChoice == '6') {
+
+        ellipseMode(RADIUS);
+        fill(255, 190, 0); //
+        ellipse(mouseX, mouseY, 35, 34, 26, 24);
+
+        ellipseMode(CENTER);
+        noStroke(20)
+        fill(255, 204, 0);
+        ellipse(mouseX, mouseY, 30, 30, 40, 40);
+    } else if (toolChoice == '7') {
+
+        image(img2, mouseX, mouseY);
+    } else if (toolChoice == '8') {
+        image(img3, mouseX, mouseY);
+
+    } else if (toolChoice == '9') {
+        noStroke(20)
+        ellipse(mouseX, mouseY, 53, 46, 55, 55)
+
+    } else if (toolChoice == '0') {
+        stroke(0, 0);
+        fill(random(255), random(255), random(255), random(255));
+        rect(mouseX, mouseY, 70, 130);
+    } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
+        image(img, mouseX, mouseY);
+
+    }
+}
+
 function testbox(r, g, b) {
-// this is a test function that will show you how you can put your own functions into the sketch
-  x = mouseX;
-  y = mouseY;
-  fill(r, g, b);
-  rect(x-50, y-50, 100, 100);
+    // this is a test function that will show you how you can put your own functions into the sketch
+    x = mouseX;
+    y = mouseY;
+    fill(r, g, b);
+    rect(x - 50, y - 50, 100, 100);
 
 }
 
 function clear_print() {
-// this will do one of two things, x clearsg4 the screen by resetting the background
-// p calls the routine saveme, which saves a copy of the screen
-  if (key == 'x' || key == 'X') {
-    background(255); // set the screen back to the background color
-  } else if (key == 'p' || key == 'P') {
-     saveme();  // call saveme which saves an image of the screen
-  }
-}1
+    // this will do one of two things, x clearsg4 the screen by resetting the background
+    // p calls the routine saveme, which saves a copy of the screen
+    if (key == 'x' || key == 'X') {
+        background(255); // set the screen back to the background color
+    } else if (key == 'p' || key == 'P') {
+        saveme(); // call saveme which saves an image of the screen
+    }
+}
+1
 
-function saveme(){
+function saveme() {
     //this will save the name as the intials, date, time and a millis counting number.
     // it will always be larger in value then the last one.
-  filename=initials+day() + hour() + minute() +second();
-  if (second()!=lastscreenshot) { // don't take a screenshot if you just took one
-    saveCanvas(filename, 'jpg');
-  }
-  lastscreenshot=second(); // set this to the current second so no more than one per second
-  
+    filename = initials + day() + hour() + minute() + second();
+    if (second() != lastscreenshot) { // don't take a screenshot if you just took one
+        saveCanvas(filename, 'jpg');
+    }
+    lastscreenshot = second(); // set this to the current second so no more than one per second
+
 }
--- art3/covid_game.js
+++ art3/covid_game.js
@@ -1,67 +1,67 @@
 var imgX = 40;
 var imgY = 40;
 var imgSize = 140;
-var score =0;
+var score = 0;
 var gameState = "L1";
 
 function preload() {
-  img = loadImage('https://juyeongkang.github.io/virus.png');
-  img2 = loadImage('https://juyeongkang.github.io/Mutate.png');
+    img = loadImage('https://juyeongkang.github.io/virus.png');
+    img2 = loadImage('https://juyeongkang.github.io/Mutate.png');
 }
 
 function setup() {
-  createCanvas(600, 600);
-  textAlign(CENTER);
-  textSize(20);
+    createCanvas(600, 600);
+    textAlign(CENTER);
+    textSize(20);
 } // end setup
 
 
 function draw() {
-  background(150,75,0);
-  if(gameState == "L1"){
-    levelOne();
-  }
-  if(gameState == "L2"){
-     levelTwo();
-  }
-  
-  text(("Kill Point: " + score), width/2, 40);
-  
+    background(150, 75, 0);
+    if (gameState == "L1") {
+        levelOne();
+    }
+    if (gameState == "L2") {
+        levelTwo();
+    }
+
+    text(("Kill Point: " + score), width / 2, 40);
+
 
 } // end draw
 
 
-function levelOne(){
-  text("COVID-19", width/2, height-20);
-  var distToImg= dist(imgX, imgY, mouseX, mouseY);
-  if (distToImg <imgSize/2){
-    imgX = random(width);
-    imgY= random(height);
-    score= score +1;
-  }
-  if(score>10){
-     gameState = "L2";
-  }
-  
-  
-  image(img, imgX, imgY, 50, 50);
+function levelOne() {
+    text("COVID-19", width / 2, height - 20);
+    var distToImg = dist(imgX, imgY, mouseX, mouseY);
+    if (distToImg < imgSize / 2) {
+        imgX = random(width);
+        imgY = random(height);
+        score = score + 1;
+    }
+    if (score > 10) {
+        gameState = "L2";
+    }
+
+
+    image(img, imgX, imgY, 50, 50);
 } // end level one
 
 
-function levelTwo(){
-   background(80,80,80);
-  text("Mutant COVID-19", width/2, height-20);
-  var distToImg2= dist(imgX, imgY, mouseX, mouseY);
-  if (distToImg2 <imgSize/2){
-    imgX = random(width);
-    imgY= random(height);
-    score= score +1;
-  }
-  if(score>20){
+function levelTwo() {
+    background(80, 80, 80);
+    text("Mutant COVID-19", width / 2, height - 20);
+    var distToImg2 = dist(imgX, imgY, mouseX, mouseY);
+    if (distToImg2 < imgSize / 2) {
+        imgX = random(width);
+        imgY = random(height);
+        score = score + 1;
+    }
+    if (score > 20) {
 
-  fill(random(255));
-  }
-  
-  
-  image(img2, imgX, imgY, 100, 100);
+        fill(random(255));
+    }
+
+
+    image(img2, imgX, imgY, 100, 100);
 } // end level Two
